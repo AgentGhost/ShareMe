c0c9dfe581536055b451fa5e3fab27b584cdc665
Mikel007 <michaelnormann@live.de>
2018-08-15 02:41:44 +0200

    Anlegen eines neuen Projektes, das sehr wichtig ist!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tap, map } from 'rxjs/operators';
+import { Lied, verzeichnis } from './verzeichnis';
+import * as copyToClipboard from 'copy-to-clipboard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ShareMe';
+export class AppComponent implements OnInit, AfterViewInit {
+  input = new FormControl('')
+  @ViewChild("inputElement") inputElement: ElementRef
+  songList: Lied[] = []
+
+  ngOnInit() {
+    this.input.valueChanges.pipe(
+      // map(value => value.replace(/\D/g, "")),
+      // tap(value => console.log(value)),
+    ).subscribe()
+    this.addSong("B101")
+  }
+
+  ngAfterViewInit() {
+    this.inputElement.nativeElement.focus()
+  }
+
+  onKeydownEnter() {
+    const input = this.inputElement.nativeElement.value
+    this.addSong(input)
+    this.inputElement.nativeElement.value = ""
+  }
+
+  addSong(input: string) {
+    const song = verzeichnis.find(L => {
+      return L.index.toLowerCase() === input.toLowerCase()
+    })
+    this.songList.push(song)
+  }
+
+  // share() {
+  //   navigator['share']({
+  //     title: 'Web Fundamentals',
+  //     text: 'Check out Web Fundamentals — it rocks!',
+  //     url: 'https://developers.google.com/web',
+  //   })
+  //     .then(() => console.log('Successful share'))
+  //     .catch((error) => console.log('Error sharing', error));
+  // }
+
+  copy() {
+    const text = this.songList
+      .map(L => L.index + " " + L.name)
+      .join("\n")
+    copyToClipboard(text)
+  }
+  
+  remove(index:number) {
+    this.songList.splice(index, 1)
+  }
 }
---
+++ src/app/verzeichnis.ts
@@ -0,0 +1,13 @@
+export interface Lied {
+    index: string
+    name: string
+}
+
+export const verzeichnis: Lied[] = [
+    { index: "B101", name: "Du großer Gott" },
+    { index: "B102", name: "Du großer Gott5" },
+    { index: "B104", name: "Du großer Gott4" },
+    { index: "B105", name: "Du großer Gott3" },
+    { index: "B106", name: "Du großer Gott2" },
+    { index: "B107", name: "Test" },
+]
